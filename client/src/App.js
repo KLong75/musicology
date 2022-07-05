02842ec388ec372001fadd9997639feb978bfe06
Kevin Long <98487770+KLong75@users.noreply.github.com>
2022-07-04 21:40:26 -0500

    fun with create-react-app opening page

--- client/src/App.js
+++ client/src/App.js
@@ -7,16 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          U, Me, and the Kevins
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Let's Rock!
-        </a>
+        
+          <p>Let's Rock</p>
+        
       </header>
     </div>
   );
