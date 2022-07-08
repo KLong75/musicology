6e6ba4004c30ac139ddc71598666a3f5c6cbd7d9
Kevin Heaton <kevin.heaton21@yahoo.com>
2022-07-07 19:59:00 -0500

    initial commit

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/musician-networking-app', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
+
 });
 
 module.exports = mongoose.connection;
