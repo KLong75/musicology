ca69360bba65a3f5c959a0b5ab875899be418c11
Roy4th <97925568+Roy4th@users.noreply.github.com>
2022-07-09 22:03:01 -0500

    changed components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './guitar-12793.png';
 import './App.css';
+import PostList from './Components/PostList';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           U, Me, and the Kevins
@@ -13,7 +15,8 @@ function App() {
         
           <p>Let's Rock</p>
         
-      </header>
+      </header> */}
+      <PostList/>
     </div>
   );
 }
